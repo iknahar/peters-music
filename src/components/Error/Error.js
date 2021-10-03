a81f60bd9267eab73b186578aed4ae94adf45c67
Kamrun Nahar <knahar.kamrun@gmail.com>
2021-10-04 02:00:43 +0600

    Error Pages

--- src/components/Error/Error.js
+++ src/components/Error/Error.js
@@ -1,12 +1,17 @@
-import React from 'react';
-
+import React from "react";
+import errorImage from "../../images/error.jpg";
 
 const Error = () => {
-    return (
-      <div>
-
-      </div>
-    );
+  return (
+    <div
+      style={{
+        height: "75vh",
+        background: `url(${errorImage})`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+      }}
+    ></div>
+  );
 };
 
 export default Error;
