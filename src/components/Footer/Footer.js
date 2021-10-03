b0fc6939a0a06b56d9311959e0142e3d07c05765
Kamrun Nahar <knahar.kamrun@gmail.com>
2021-10-04 01:46:21 +0600

    Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,30 @@
-import React from 'react';
-
+import React from "react";
+import "./Footer.css";
 const Footer = () => {
-    return (
-        <div>
-            <h1>Footer</h1>
+  return (
+    <div className="foot px-5">
+      <div className="d-flex align-items-center justify-content-between">
+        <div className="footerP">
+          <h4>Peter's Music School</h4>
+          <p>
+            Address: Dhaka, Bangladesh, Bottola, Music School Goli
+          </p>
         </div>
-    );
+
+        <div className="footerP">
+          <p>
+            Check about out students and teachers. Music has always been in our
+            soul and we are trying to spread it since 1988.
+          </p>
+        </div>
+        <div className="footerP">
+          Visit us for more info, or follow us on social media, Admission going
+          on! So hurry up!
+        </div>
+      </div>
+      <p className="right">All right reserve © 2021</p>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -9,16 +9,16 @@ const Hero = () => {
         style={{ background: `url(${bg})`, backgroundAttachment: "fixed" }}
       >
         <div className="herotext">
-          <h1 className='large'>Ready To Play</h1>
-          <p className='py-4'>
+          <h1 className="large">Ready To Play</h1>
+          <p className="py-4">
             It was founded 1905 and existed up to 1939. The fabrication was
             located in Warschauerstraße 70 and Boxhagenerstraße 16a in Berlin.
             The owners Paul Richard Bogs (1871-1949) and Adolf Ernst Voigt were
             awarded an imperial and royal warrant of appointment to the courts
             of Austria-Hungary and Spain. Voigt left the company in 1913, Bogs
-            went on until the company was closed in 1939. The company built
-            about 66.000 instruments.
+            went on until the company was closed in 1939. 
           </p>
+          <button className="btnHero">Visit Our Website</button>
         </div>
       </div>
     );
