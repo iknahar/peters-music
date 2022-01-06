d012a730d139b81814d2eb625fa24e073d931d82
Kamrun Nahar <knahar.kamrun@gmail.com>
2022-01-06 21:57:58 +0600

    Hero Section Responsive

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -12,12 +12,11 @@ const Hero = () => {
       <div className="herotext">
         <h1 className="large">Ready To Play</h1>
         <p className="py-4">
-          It was founded 1905 and existed up to 1939. The fabrication was
-          located in Warschauerstraße 70 and Boxhagenerstraße 16a in Berlin. The
-          owners Paul Richard Bogs (1871-1949) and Adolf Ernst Voigt were
+          It was founded 1905. The fabrication was
+          located in Warschauer and Boxhagenerstraße 16a in Berlin. The
+          owners Paul Richard Bogs and Adolf Ernst Voigt were
           awarded an imperial and royal warrant of appointment to the courts of
-          Austria-Hungary and Spain. Voigt left the company in 1913, Bogs went
-          on until the company was closed in 1939.
+          Austria-Hungary and Spain. 
         </p>
         <Link className="btnHero" to="/about">
           Learn More
